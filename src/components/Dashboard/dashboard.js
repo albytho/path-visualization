d2242d130d67b8218d47bddcba11aab72f686d88
Alby Thomas <Alby@Albys-MacBook-Pro.local>
2020-07-19 18:35:06 -0500

    Added button placeholders on navbar

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -7,8 +7,26 @@ export default class dashboard extends Component {
     render() {
         return (
             <div className='dashboard'>
-                <nav className="navbar navbar-dark bg-primary">
-                    <span className="navbar-brand mb-0 h1">Search Algorithm Visualization</span>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <a className="navbar-brand" href="#">Search Algorithm Visualization</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div>
+                        <div className="navbar-nav">
+                            <div className="dropdown">
+                                <button className="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Select Algorithm
+                                </button>
+                                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a className="dropdown-item">Depth First Search</a>
+                                    <a className="dropdown-item">Breadth First Search</a>
+                                </div>
+                            </div>
+                            <button type="button" className="btn btn-success">Animate</button>
+                            <button type="button" className="btn btn-danger">Reset</button>
+                        </div>
+                    </div>
                 </nav>
                 <Grid/>
             </div>
